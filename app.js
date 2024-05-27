b8f732de60117af903797764b6d96fb3ae22df1b
Besdicode <besdiseyidovaa@gmail.com>
2024-05-27 11:41:25 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// let a = +prompt("bir eded daxil edin");
+
+// if (a%2==0) {
+//     console.log('eded cutdur');
+// }else{
+//     console.log('eded tekdir'); 
+// }
+
+// let ad = prompt("adinizi daxil edin");
+// let parol = prompt("parolonuzu daxil edin");
+
+// if( ad == "eli" && parol == "12345"){
+//     console.log("sehfeye xos gelmisiniz");
+// }
+// else if(ad == "eli" && parol != "12345"){
+//     console.log("parol false");
+// }
+// else if(ad != "eli" && parol == "12345"){
+//     console.log("ad false");
+// }
+// else{
+//     console.log("sehv");
+// }
